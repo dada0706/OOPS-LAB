24b56c3be1972ead5cafd7a3b7c4cbd9a97a3cac
dada0706 <dadukolimi75@gmail.com>
2025-04-04 22:32:47 +0530

    Update PatientConstructor.java

--- 5.POLYMORPHISM/CONSTRUCTOR PROGRAMS/PatientConstructor.java
+++ 5.POLYMORPHISM/CONSTRUCTOR PROGRAMS/PatientConstructor.java
@@ -16,7 +16,7 @@ class Patient {
         System.out.println("Patient Name: " + name + ", Age: " + age);
     }
 }
-public class main PatientCostructor{
+public class main PatientConstructor{
     public static void main(String[] args) {
         java.util.Scanner sc = new java.util.Scanner(System.in);
 
